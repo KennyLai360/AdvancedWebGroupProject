2addda45cd12f4272c6b0886f34b477200d7ed99
ao00119 <ao00119@UCS211.surrey.ac.uk>
2016-03-08 17:21:10 +0000

    Configured project in NetBean environment

--- src/main/java/com/surrey/com3014/group10/controllers/IndexController.java
+++ src/main/java/com/surrey/com3014/group10/controllers/IndexController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class IndexController {
-    @RequestMapping("/index")
+    @RequestMapping("/")
     public String index(@RequestParam(value = "name", required = false, defaultValue = "World") String name, Model model) {
         model.addAttribute("name", name);
         return "index";
