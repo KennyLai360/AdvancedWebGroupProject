5f61e238a21e3bfee4642a186c712e9aeadce050
maudff <adekunle.oladejo@ecrebo.com>
2016-03-09 15:53:38 +0000

    Fixed Project settings to be compatible with NetBeans IDE

--- src/main/java/com/surrey/com3014/group10/controllers/IndexController.java
+++ src/main/java/com/surrey/com3014/group10/controllers/IndexController.java
@@ -1,15 +1,17 @@
 package com.surrey.com3014.group10.controllers;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
-public class IndexController {
+public class IndexController 
+{
+    
     @RequestMapping("/index")
     public String index(@RequestParam(value = "name", required = false, defaultValue = "World") String name, Model model) {
         model.addAttribute("name", name);
+        System.out.println("name: ===> " + name);
         return "index";
     }
 }
