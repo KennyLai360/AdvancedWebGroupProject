6c337ce28b8504e6b47d208fef4957d46701f63c
KENNY <kennylai360@gmail.com>
2016-04-30 19:40:17 +0100

    Added icons and button cursor and added drawing code

--- src/main/webapp/WEB-INF/jsp/game.jsp
+++ src/main/webapp/WEB-INF/jsp/game.jsp
@@ -1,88 +1,46 @@
-<!doctype html>
-
+<html>
 <jsp:include page="../templates/headerTemplate.jsp" />
-<script type="text/javascript" src="../../js/draw.js"></script>
+
 <body>
+    <script type="text/javascript" src="../../js/draw.js"></script>
 
-<div class="container">
-<div class="row">
-    <div class="col-md-3">
-        <h5><b>Users in-game</b></h5>
-        <div style="border: black 1px solid; height:250px; border-radius: 25px">
-            <ul>
-                <li>
-                    User1
-                </li>
-                <li>
-                    User2
-                </li>
-                <li>
-                    User3
-                </li>
-                <li>
-                    User4
-                </li>
-                <li>
-                    User5
-                </li>
-                <li>
-                    User6
-                </li>
-                <li>
-                    User7
-                </li>
-                <li>
-                    User8
-                </li>
-            </ul>
-        </div>
-        <h5><b>Chat</b></h5>
-        <div style="border: black 1px solid; height:250px; border-radius: 25px">
-            <div style="padding:5px;">
-                <p>[15:13:42] Doge</p>
-                <p>[15:13:45] Bag</p>
-                <p>[15:13:56] Bob</p>
-                <p>[15:13:59] Cat</p>
-                <p>[15:14:01] wat</p>
-
+<div class="container" style="padding-top:30px;">
+    <div class="row">
+        <div class="col-md-3">
+            <div style="border: black 1px solid; height:500px; border-radius: 25px">
+                <!--User in game info here-->
             </div>
         </div>
+
+        <div class="col-md-offset-3" id="canvasDiv" style="border: black 1px solid;height:500px; border-radius: 25px">
+        </div>
+
     </div>
-    <div class="col-md-7">
-        <h5><b>XXXXX is drawing...</b></h5>
-        <canvas style="border: 1px solid; border-radius:25px;" height="500" width="650" id="drawing-canvas"></canvas>
-        <div class="input-group" style="padding-top:10px">
+        <div class="row">
+            <div class="col-md-3" style="border: black 1px solid; height:50px;  border-radius: 25px;">
+                <!-Some other info icon etc etc here?-->
+            </div>
+            <div class="input-group col-md-9" style="">
             <input type="text" class="form-control" placeholder="Send some message here!">
                     <span class="input-group-btn">
                         <button class="btn btn-secondary" type="button">Send</button>
                     </span>
         </div>
-    </div>
-    <div class="col-md-2">
-        <h5><b>Draw settings</b></h5>
-            <%--<button id="drawing-mode" class="btn btn-info">Enter drawing mode</button>--%>
-            <div style="display: none;" id="drawing-mode-options">
-                <%--<label for="drawing-mode-selector">Mode:</label>--%>
-                <%--<select id="drawing-mode-selector">--%>
-                    <%--<option>Pencil</option>--%>
-                    <%--<option>Circle</option>--%>
-                    <%--<option>Spray</option>--%>
-                    <%--<option>Pattern</option>--%>
-
-                    <%--<option>hline</option>--%>
-                    <%--<option>vline</option>--%>
-                    <%--<option>square</option>--%>
-                    <%--<option>diamond</option>--%>
-                <%--</select><br>--%>
-                <label for="drawing-line-width">Line width:</label>
-                <input type="range" value="10" min="0" max="150" id="drawing-line-width"><br>
-                <label for="drawing-color">Line color:</label>
-                <input type="color" value="#000000" id="drawing-color"><br>
-            </div>
-    </div>
+        </div>
 
 </div>
+
+<div class="container">
+    <div class="row">
+        <jsp:include page="../templates/footerTemplate.jsp" />
+    </div>
 </div>
+
+<div  style="padding: 10px;"></div>
+
+<script type="text/javascript">
+    prepareCanvas();
+</script>
 
 </body>
 
--- src/main/webapp/js/draw.js
+++ src/main/webapp/js/draw.js
@@ -1,139 +1,69 @@
-window.onload = function(){
+function prepareCanvas() {
+    var canvasDiv = document.getElementById('canvasDiv');
+    canvas = document.createElement('canvas');
+    canvas.setAttribute('width', $("#canvasDiv").width());
+    canvas.setAttribute('height', $("#canvasDiv").height());
+    canvas.setAttribute('id', 'canvas');
+    canvasDiv.appendChild(canvas);
+    if(typeof G_vmlCanvasManager != 'undefined') {
+        canvas = G_vmlCanvasManager.initElement(canvas);
+    }
+    context = canvas.getContext("2d");
 
-    var canvas = new fabric.Canvas('drawing-canvas');
-    var
-        //drawingModeEl = document.getElementById('drawing-mode'),
-        drawingOptionsEl = document.getElementById('drawing-mode-options'),
-        drawingColorEl = document.getElementById('drawing-color'),
-        drawingLineWidthEl = document.getElementById('drawing-line-width');
+    $('#canvas').mousedown(function(e){
+        var mouseX = e.pageX - this.offsetLeft;
+        var mouseY = e.pageY - this.offsetTop;
 
-    canvas.isDrawingMode = true;
+        paint = true;
+        addClick(e.pageX - this.offsetLeft, e.pageY - this.offsetTop);
+        redraw();
+    });
 
-    //drawingModeEl.onclick = function() {
-        //canvas.isDrawingMode = !canvas.isDrawingMode;
-        if (canvas.isDrawingMode) {
-            //drawingModeEl.innerHTML = 'Close drawing settings';
-            drawingOptionsEl.style.display = '';
+    $('#canvas').mousemove(function(e){
+        if(paint){
+            addClick(e.pageX - this.offsetLeft, e.pageY - this.offsetTop, true);
+            redraw();
         }
-        else {
-            //drawingModeEl.innerHTML = 'Show drawing settings';
-            drawingOptionsEl.style.display = 'none';
-        }
-    //};
+    });
 
-    //if (fabric.PatternBrush) {
-    //    var vLinePatternBrush = new fabric.PatternBrush(canvas);
-    //    vLinePatternBrush.getPatternSrc = function() {
-    //
-    //        var patternCanvas = fabric.document.createElement('canvas');
-    //        patternCanvas.width = patternCanvas.height = 10;
-    //        var ctx = patternCanvas.getContext('2d');
-    //
-    //        ctx.strokeStyle = this.color;
-    //        ctx.lineWidth = 5;
-    //        ctx.beginPath();
-    //        ctx.moveTo(0, 5);
-    //        ctx.lineTo(10, 5);
-    //        ctx.closePath();
-    //        ctx.stroke();
-    //
-    //        return patternCanvas;
-    //    };
-    //
-    //    var hLinePatternBrush = new fabric.PatternBrush(canvas);
-    //    hLinePatternBrush.getPatternSrc = function() {
-    //
-    //        var patternCanvas = fabric.document.createElement('canvas');
-    //        patternCanvas.width = patternCanvas.height = 10;
-    //        var ctx = patternCanvas.getContext('2d');
-    //
-    //        ctx.strokeStyle = this.color;
-    //        ctx.lineWidth = 5;
-    //        ctx.beginPath();
-    //        ctx.moveTo(5, 0);
-    //        ctx.lineTo(5, 10);
-    //        ctx.closePath();
-    //        ctx.stroke();
-    //
-    //        return patternCanvas;
-    //    };
-    //
-    //    var squarePatternBrush = new fabric.PatternBrush(canvas);
-    //    squarePatternBrush.getPatternSrc = function() {
-    //
-    //        var squareWidth = 10, squareDistance = 2;
-    //
-    //        var patternCanvas = fabric.document.createElement('canvas');
-    //        patternCanvas.width = patternCanvas.height = squareWidth + squareDistance;
-    //        var ctx = patternCanvas.getContext('2d');
-    //
-    //        ctx.fillStyle = this.color;
-    //        ctx.fillRect(0, 0, squareWidth, squareWidth);
-    //
-    //        return patternCanvas;
-    //    };
-    //
-    //    var diamondPatternBrush = new fabric.PatternBrush(canvas);
-    //    diamondPatternBrush.getPatternSrc = function() {
-    //
-    //        var squareWidth = 10, squareDistance = 5;
-    //        var patternCanvas = fabric.document.createElement('canvas');
-    //        var rect = new fabric.Rect({
-    //            width: squareWidth,
-    //            height: squareWidth,
-    //            angle: 45,
-    //            fill: this.color
-    //        });
-    //
-    //        var canvasWidth = rect.getBoundingRectWidth();
-    //
-    //        patternCanvas.width = patternCanvas.height = canvasWidth + squareDistance;
-    //        rect.set({ left: canvasWidth / 2, top: canvasWidth / 2 });
-    //
-    //        var ctx = patternCanvas.getContext('2d');
-    //        rect.render(ctx);
-    //
-    //        return patternCanvas;
-    //    };
-    //}
+    $('#canvas').mouseup(function(e){
+        paint = false;
+    });
 
-    //document.getElementById('drawing-mode-selector').addEventListener('change', function() {
-    //
-    //    //if (this.value === 'hline') {
-    //    //    canvas.freeDrawingBrush = vLinePatternBrush;
-    //    //}
-    //    //else if (this.value === 'vline') {
-    //    //    canvas.freeDrawingBrush = hLinePatternBrush;
-    //    //}
-    //    //else if (this.value === 'square') {
-    //    //    canvas.freeDrawingBrush = squarePatternBrush;
-    //    //}
-    //    //else if (this.value === 'diamond') {
-    //    //    canvas.freeDrawingBrush = diamondPatternBrush;
-    //    //}
-    //    //else if (this.value === 'texture') {
-    //    //    canvas.freeDrawingBrush = texturePatternBrush;
-    //    //}
-    //    //else {
-    //        canvas.freeDrawingBrush = new fabric[this.value + 'Brush'](canvas);
-    //    //}
-    //
-    //    if (canvas.freeDrawingBrush) {
-    //        canvas.freeDrawingBrush.color = drawingColorEl.value;
-    //        canvas.freeDrawingBrush.width = parseInt(drawingLineWidthEl.value, 10) || 1;
-    //    }
-    //});
+    $('#canvas').mouseleave(function(e){
+        paint = false;
+    });
 
-    drawingColorEl.onchange = function() {
-        canvas.freeDrawingBrush.color = drawingColorEl.value;
-    };
-    drawingLineWidthEl.onchange = function() {
-        canvas.freeDrawingBrush.width = parseInt(drawingLineWidthEl.value, 10) || 1;
-    };
+    var clickX = new Array();
+    var clickY = new Array();
+    var clickDrag = new Array();
+    var paint;
 
-    if (canvas.freeDrawingBrush) {
-        canvas.freeDrawingBrush.color = drawingColorEl.value;
-        canvas.freeDrawingBrush.width = parseInt(drawingLineWidthEl.value, 10) || 1;
+    function addClick(x, y, dragging)
+    {
+        clickX.push(x);
+        clickY.push(y);
+        clickDrag.push(dragging);
     }
 
-};
+    function redraw(){
+        context.clearRect(0, 0, context.canvas.width, context.canvas.height); // Clears the canvas
+
+        context.strokeStyle = "#000000";
+        context.lineJoin = "round";
+        context.lineWidth = 5;
+
+        for(var i=0; i < clickX.length; i++) {
+            context.beginPath();
+            if(clickDrag[i] && i){
+                context.moveTo(clickX[i-1], clickY[i-1]);
+            }
+            else{
+                context.moveTo(clickX[i]-1, clickY[i]);
+            }
+            context.lineTo(clickX[i], clickY[i]);
+            context.closePath();
+            context.stroke();
+        }
+    }
+}
