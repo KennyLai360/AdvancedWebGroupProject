e418c1bde50507876080069347ab69f47714388e
KENNY <kennylai360@gmail.com>
2016-05-08 19:08:07 +0100

    Added in Medals with getters and setters

--- src/main/java/com/surrey/com3014/group10/User/model/User.java
+++ src/main/java/com/surrey/com3014/group10/User/model/User.java
@@ -2,133 +2,124 @@ package com.surrey.com3014.group10.User.model;
 
 import com.surrey.com3014.group10.User.Medal;
 import com.surrey.com3014.group10.User.model.UserRole;
+
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.FetchType;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.JoinTable;
-import javax.persistence.ManyToMany;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 @Entity
-@Table(name="APP_USER")
+@Table(name = "APP_USER")
 public class User {
 
-	@Id @GeneratedValue(strategy=GenerationType.IDENTITY)
-	private int id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
 
-	@Column(name="SSO_ID", unique=true, nullable=false)
-	private String ssoId;
+    @Column(name = "SSO_ID", unique = true, nullable = false)
+    private String ssoId;
 
-	@Column(name="PASSWORD", nullable=false)
-	private String password;
+    @Column(name = "PASSWORD", nullable = false)
+    private String password;
 
-        @Column(name="SCORE", nullable=false)
-	private int score;
+    @Column(name = "SCORE", nullable = false)
+    private int score;
 
-	@ManyToMany(fetch = FetchType.EAGER)
-	@JoinTable(name = "APP_USER_USER_PROFILE",
-             joinColumns = { @JoinColumn(name = "USER_ID") },
-             inverseJoinColumns = { @JoinColumn(name = "USER_PROFILE_ID") })
-	private Set<UserRole> userProfiles = new HashSet<UserRole>();
-        
-        @Column(name="MEDAL", nullable=false)
-	private Medal medal = Medal.NONE;
-        
-        public Medal getMedal()
-        {
-            return this.medal;
-        }
-        
-        public void setMedal(Medal medal)
-        {
-            this.medal = medal;
-        }
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(name = "APP_USER_USER_PROFILE",
+        joinColumns = {@JoinColumn(name = "USER_ID")},
+        inverseJoinColumns = {@JoinColumn(name = "USER_PROFILE_ID")})
+    private Set<UserRole> userProfiles = new HashSet<UserRole>();
 
-	public int getId() {
-		return id;
-	}
+    @Column(name = "MEDAL", nullable = false)
+    @Enumerated(EnumType.STRING)
+    private Medal medal = Medal.NONE;
 
-	public void setId(int id) {
-		this.id = id;
-	}
+    @Enumerated(EnumType.STRING)
+    public Medal getMedal() {
+        return this.medal;
+    }
 
-	public String getSsoId() {
-		return ssoId;
-	}
+    public void setMedal(Medal medal) {
+        this.medal = medal;
+    }
 
-	public void setSsoId(String ssoId) {
-		this.ssoId = ssoId;
-	}
+    public int getId() {
+        return id;
+    }
 
-	public String getPassword() {
-		return password;
-	}
+    public void setId(int id) {
+        this.id = id;
+    }
 
-	public void setPassword(String password) {
-		this.password = password;
-	}
+    public String getSsoId() {
+        return ssoId;
+    }
 
-	public Set<UserRole> getUserProfiles() {
-		return userProfiles;
-	}
+    public void setSsoId(String ssoId) {
+        this.ssoId = ssoId;
+    }
 
-	public void setUserProfiles(Set<UserRole> userProfiles) {
-		this.userProfiles = userProfiles;
-	}
+    public String getPassword() {
+        return password;
+    }
 
-        public int getScore()
-        {
-            return this.score;
-        }
+    public void setPassword(String password) {
+        this.password = password;
+    }
 
-        public void setScore(int score)
-        {
-            this.score = score;
-        }
+    public Set<UserRole> getUserProfiles() {
+        return userProfiles;
+    }
+
+    public void setUserProfiles(Set<UserRole> userProfiles) {
+        this.userProfiles = userProfiles;
+    }
+
+    public int getScore() {
+        return this.score;
+    }
+
+    public void setScore(int score) {
+        this.score = score;
+    }
 
 
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + id;
-		result = prime * result + ((ssoId == null) ? 0 : ssoId.hashCode());
-		return result;
-	}
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + id;
+        result = prime * result + ((ssoId == null) ? 0 : ssoId.hashCode());
+        return result;
+    }
 
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (!(obj instanceof User))
-			return false;
-		User other = (User) obj;
-		if (id != other.id)
-			return false;
-		if (ssoId == null) {
-			if (other.ssoId != null)
-				return false;
-		} else if (!ssoId.equals(other.ssoId))
-			return false;
-		return true;
-	}
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (!(obj instanceof User))
+            return false;
+        User other = (User) obj;
+        if (id != other.id)
+            return false;
+        if (ssoId == null) {
+            if (other.ssoId != null)
+                return false;
+        } else if (!ssoId.equals(other.ssoId))
+            return false;
+        return true;
+    }
 
-	@Override
-	public String toString() {
-		return "User [id=" + id + ", ssoId=" + ssoId + ", password=" + password
-				+  ", userProfiles=" + userProfiles +"]";
-	}
+    @Override
+    public String toString() {
+        return "User [id=" + id + ", ssoId=" + ssoId + ", password=" + password
+            + ", userProfiles=" + userProfiles + "]";
+    }
 
 
 }
